1c782d7f0ad1eeb7ab674633e2b06b3ab807ce96
Oleksii Shevchenko <uzlabini@gmail.com>
2023-03-20 20:42:00 +0100

    14141

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import { GlobalStyle } from "./GlobalStyle";
+
+export const App = () => {
+  return (
+    <div>
+      React homework template
+      <GlobalStyle />
+    </div>
+  );
+};
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './components/GlobalStyle';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/DZ-5-React/">
+    <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
